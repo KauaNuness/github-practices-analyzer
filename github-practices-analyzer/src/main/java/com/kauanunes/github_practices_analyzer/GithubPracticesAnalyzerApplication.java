f209836bb4db4882b36d3e6b2227d8ffc41a9ae8
kauanuness <kaua.souza@sptech.school>
2026-02-04 21:34:02 -0300

    chore(project): cria estrutura inicial de pacotes e camadas da aplicação

---
+++ github-practices-analyzer/src/main/java/com/kauanunes/github_practices_analyzer/GithubPracticesAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.kauanunes.github_practices_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubPracticesAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubPracticesAnalyzerApplication.class, args);
+	}
+
+}
---
+++ github-practices-analyzer/src/test/java/com/kauanunes/github_practices_analyzer/GithubPracticesAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kauanunes.github_practices_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubPracticesAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
